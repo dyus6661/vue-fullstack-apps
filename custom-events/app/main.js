b1446582b57759d0fda3b65233622e898988d121
dyus6661 <dyus6661@gmail.com>
2021-08-08 13:25:17 +0300

    custom-events: Added "EventBus" mechanism

--- custom-events/app/main.js
+++ custom-events/app/main.js
@@ -1,3 +1,5 @@
+const EventBus = new Vue();
+
 const inputComponent = {
     props: {
         placeholder: {
@@ -18,7 +20,7 @@ const inputComponent = {
     },
     methods: {
         monitorEnterkey() {
-            this.$emit("add-note", {
+            EventBus.$emit("add-note", {
                 note: this.input,
                 timestamp: (new Date).toLocaleString()
             });
@@ -26,10 +28,24 @@ const inputComponent = {
         }
     }
 };
+
+const noteCountComponent = {
+    template: `<div lass="note-count">Note ount: <strong>{{ noteCount }}</strong></div>`,
+    data() {
+        return {
+            noteCount: 0
+        };
+    },
+    created() {
+        EventBus.$on("add-note", event => this.noteCount++);
+    }
+};
+
 new Vue({
     el: '#app',
     components: {
-        'input-component': inputComponent
+        "input-component": inputComponent,
+        "note-count-component": noteCountComponent
     },
     data: {
         notes: [],
@@ -41,5 +57,8 @@ new Vue({
             this.notes.push($event.note);
             this.timestamps.push($event.timestamp);
         }
+    },
+    created() {
+        EventBus.$on("add-note", event => this.addNote(event));
     }
 });
