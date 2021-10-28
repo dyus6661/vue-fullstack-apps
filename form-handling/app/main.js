3b825a9420d4b3325048909bc8297e80ed134056
dyus6661 <dyus6661@gmail.com>
2021-10-28 20:43:25 +0300

    Updated "InputForm" component to use v-model

--- form-handling/app/main.js
+++ form-handling/app/main.js
@@ -3,16 +3,21 @@ const InputForm = {
   <div class="input-form">
     <form @submit="submitForm" class="ui form">
       <div class="field">
-        <input ref="newItem" type="text" placeholder="Add an item!">
+        <input v-model="newItem" type="text" placeholder="Add an item!">
       </div>
       <button class="ui button">Submit</button>
     </form>
-  <div>  
+  </div>  
   `,
+  data() {
+    return {
+      newItem: ""
+    }
+  },
   methods: {
     submitForm(e) {
       e.preventDefault();
-      console.log(this.$refs.newItem.value);
+      console.log(this.newItem);
     }
   }
 };
