c2fce7d5eb0959d3b3d2f21c453ba797d8329fe5
dyus6661 <dyus6661@gmail.com>
2021-10-28 20:52:05 +0300

    Added ability to save and render items

--- form-handling/app/main.js
+++ form-handling/app/main.js
@@ -7,17 +7,26 @@ const InputForm = {
       </div>
       <button class="ui button">Submit</button>
     </form>
+    <div class="ui segment">
+      <h4 class="ui header">Items</h4>
+      <ul v-if="items.length">
+        <li v-for="item in items" class="item">{{ item }}</li>
+      </ul>
+      <p v-else>There is no items in the array!</p>
+    </div>
   </div>  
   `,
   data() {
     return {
-      newItem: ""
+      newItem: "",
+      items: []
     }
   },
   methods: {
     submitForm(e) {
+      this.items.push(this.newItem);
+      this.newItem = "";
       e.preventDefault();
-      console.log(this.newItem);
     }
   }
 };
