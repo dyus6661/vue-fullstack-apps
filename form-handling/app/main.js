f15f7bd0be111bb52611ca04e36446340012e8d9
dyus6661 <dyus6661@gmail.com>
2021-10-29 17:53:59 +0300

    Added validation to the "InputForm" component

--- form-handling/app/main.js
+++ form-handling/app/main.js
@@ -5,10 +5,17 @@ const InputForm = {
       <div class="field">
         <label>New item</label>
         <input v-model="fields.newItem" type="text" placeholder="Add an item!">
+        <span style="float: right">{{ fields.newItem.length }}/20</span>
+        <span style="color: red;">{{ fieldErrors.newItem }}</span>
+        <span v-if="isNewItemInputLimitExceeded"
+        style="color: red; display: block">
+          Must be under twenty characters
+        </span>
       </div>
       <div class="field">
         <label>Email</label>
         <input v-model="fields.email" type="text" placeholder="What's your email?">
+        <span style="color: red;">{{ fieldErrors.email }}</span>
       </div>
       <div class="field">
         <label>Urgency</label>
@@ -18,14 +25,22 @@ const InputForm = {
           <option>Moderate</option>
           <option>Urgent</option>        
         </select>
+        <span style="color: red;">{{ fieldErrors.urgency }}</span>
+        <span v-if="isNotUrgent"
+        style="color: red; display: block">
+          Must be moderate to urgent
+        </span>
       </div>
       <div class="field">
         <div class="ui checkbox">
           <input v-model="fields.termsAndConditions" type="checkbox" />
           <label>I accept the terms and conditions</label>
+          <span style="color: red;">{{ fieldErrors.termsAndConditions }}</span>
         </div>
       </div>
-      <button class="ui button">Submit</button>
+      <button class="ui button"
+              :disabled="isNewItemInputLimitExceeded || isNotUrgent"
+      >Submit</button>
     </form>
     <div class="ui segment">
       <h4 class="ui header">Items</h4>
@@ -44,14 +59,54 @@ const InputForm = {
         urgency: "",
         termsAndConditions: false
       },
+      fieldErrors: {
+        newItem: null,
+        email: null,
+        urgency: null,
+        termsAndConditions: null
+      },
       items: []
     }
   },
   methods: {
     submitForm(e) {
-      this.items.push(this.newItem);
-      this.newItem = "";
       e.preventDefault();
+
+      this.fieldErrors = this.validateForm(this.fields);
+      if (Object.keys(this.fieldErrors).length) return;
+
+      this.items.push(this.fields.newItem);
+
+      this.fields.newItem = "";
+      this.fields.email = "";
+      this.fields.urgency = "";
+      this.fields.termsAndConditions = false;
+    },
+    validateForm(fields) {
+      const errors = {};
+      if (!fields.newItem) errors.newItem = "New Item Required";
+      if (!fields.email) errors.email = "Email Required";
+      if (!fields.urgency) errors.urgency = "Urgency Required";
+      if (!fields.termsAndConditions) {
+        errors.termsAndConditions = "Terms and conditions have to be approved";
+      }
+      if (fields.email && !this.isEmail(fields.email)) {
+        errors.email = "Invalid Email";
+      }
+
+      return errors;
+    },
+    isEmail(email) {
+      const re = /\S+@\S+\.\S+/;
+      return re.test(email);
+    }
+  },
+  computed: {
+    isNewItemInputLimitExceeded() {
+      return this.fields.newItem.length > 20;
+    },
+    isNotUrgent() {
+      return this.fields.urgency === "Nonessential";
     }
   }
 };
